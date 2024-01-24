b8cebbd28f90be82ebdd82e41edbcc4932835520
Marta Łoskot <loskotmarta@gmail.com>
2024-01-24 16:10:04 +0100

    Add left function

--- js/app.js
+++ js/app.js
@@ -1,34 +1,89 @@
+
+
+
+
 const bob = document.querySelector('.bob')
-const eyes = document.querySelector('.eyes-container')
+const eye = document.querySelector('.eye')
 
 let left = 50
 
-function moveRight() {
+function moveRight(){
+    bob.style.left = left + 'px'
     left += 50
-    bob.style.left = left +'px'
-    eyes.style.transform = 'rotate(0)'
-    eyes.style.top = '0'
 }
 
-function moveLeft() {
-    left -= 50
-    bob.style.left = left +'px'
-    eyes.style.transform = 'rotateY(180deg)'
+function moveLeft(){
+    bob.style.left = -left + 'px'
+    left += 50
 }
 
 function moveBob(e) {
-    const keyArrow = e.key
-    console.log(keyArrow)
+    const key = e.key
+    console.log(e.key)
 
-        if (keyArrow === 'ArrowRight') {
-            moveRight()
-        }
-        if (keyArrow === 'ArrowLeft') {
-            moveLeft()
-        }
+    if (key === 'ArrowLeft') {
+        moveLeft()
+    }
+
+    if (key === 'ArrowRight') {
+        moveRight()
+    }
 }
 
 document.addEventListener('keydown', moveBob)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const bob = document.querySelector('.bob')
+// const eyes = document.querySelector('.eyes-container')
+
+// let left = 50
+
+// function moveRight() {
+//     left += 50
+//     bob.style.left = left +'px'
+//     eyes.style.transform = 'rotate(0)'
+//     eyes.style.top = '0'
+// }
+
+// function moveLeft() {
+//     left -= 50
+//     bob.style.left = left +'px'
+//     eyes.style.transform = 'rotateY(180deg)'
+// }
+
+// function moveBob(e) {
+//     const keyArrow = e.key
+//     console.log(keyArrow)
+
+//         if (keyArrow === 'ArrowRight') {
+//             moveRight()
+//         }
+//         if (keyArrow === 'ArrowLeft') {
+//             moveLeft()
+//         }
+// }
+
+// document.addEventListener('keydown', moveBob)
 
 
 
